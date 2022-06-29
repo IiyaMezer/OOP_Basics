b68e6bfa169eff5eca36b6f8e5b56412e8e16773
Mezer_Ilya <mr.mezer@myttk.ru>
2022-06-29 21:40:17 +0700

    созданы поля id cash type

--- HW_2/Account.cs
+++ HW_2/Account.cs
@@ -1,7 +1,34 @@
 ﻿namespace HW_2
 {
+    
     internal class Account
     {
+        private int _Id;
+        private decimal _Cash;
+        private enum _Type
+        {
+            VIP = 1, //особые условие
+            Normal = 2, //обычные условия
+            Corp = 3, //корпоративные условия(зарплатная карта)
+            Comp = 4, //счет компании
+        }  
+
+        public int Id
+        { 
+            get => _Id; 
+            set => _Id = value; 
+        }
+        public decimal Cash 
+        {
+            get => _Cash;
+            set => _Cash = value;
+        }
+
+        public void Writeinfo(int id, decimal cash, int Type)
+        {
+            Console.WriteLine()
+        }
+        
 
     }
 }
