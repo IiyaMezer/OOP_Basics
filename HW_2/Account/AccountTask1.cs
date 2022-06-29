d21e0d39e9be1c044da7fddfa24c39a313f26995
Mezer_Ilya <mr.mezer@myttk.ru>
2022-06-29 22:49:31 +0700

    Задание 1 готово.

---
+++ HW_2/Account/AccountTask1.cs
@@ -0,0 +1,43 @@
+﻿
+namespace HW_2.Account;
+ internal class AccountTask1
+ {
+    private int _Id;
+    private decimal _Cash;
+    private TypeEnum _Type;
+
+    public int Id
+        {
+            get => _Id;
+            set => _Id = value;
+        }
+    public decimal Cash
+        {
+            get => _Cash;
+            set => _Cash = value;
+        }
+
+    public TypeEnum Type
+    {
+        get => _Type;
+        set => _Type = value;
+    }
+
+
+   
+    public void Writeinfo(int id, decimal cash, TypeEnum type)
+    {
+        int Id = _Id;
+        decimal Cash = _Cash;
+        TypeEnum Type = type;
+
+
+        Console.WriteLine($"Номер счета: {id};");
+        Console.WriteLine($"Баланс {cash} руб. ;");
+        Console.WriteLine($"Тип {type};");
+        Console.WriteLine("");
+    }
+
+
+    }
+
