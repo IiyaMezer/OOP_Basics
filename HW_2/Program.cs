d31faefc7ad437664e8df368549432fb8353617c
Mezer_Ilya <mr.mezer@myttk.ru>
2022-07-06 18:09:14 +0700

    Задание 2 готово

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -1,5 +1,19 @@
 ﻿using OOP_Basics.Account;
 
+
+static string StringReverse(string source)
+{
+    char[] chars = source.ToCharArray();
+    string reverse = "";
+    // Не самый лучший способ, но зато поработал с массивом
+    for (int i = chars.Length - 1; i > -1; i--)
+    {
+        reverse += chars[i];
+    }
+
+    return reverse;
+}
+
  Account Acc1 = new(10M, TypeEnum.VIP);
  Account Acc2 = new(10M, TypeEnum.Normal);
  Account Acc3 = new(10M, TypeEnum.Corp);
@@ -22,7 +36,7 @@ Console.WriteLine(Acc1.Cash);//проверка баланса
 Acc1.CashOut(30M);//пытаемся снять в пределах допустимого
 Console.WriteLine(Acc1.Cash);//проверка баланса
 Console.WriteLine("========================");
-Acc4.Transfer(Acc1, 1M);
+Acc4.Transfer(Acc1, 1M);///перевод денег
 Console.WriteLine(Acc4.Cash);//проверка баланса
 Console.WriteLine(Acc1.Cash);//проверка баланса
 
@@ -32,7 +46,11 @@ Acc1.Writeinfo();
 Acc2.Writeinfo();
 Acc3.Writeinfo();
 Acc4.Writeinfo();
+Console.WriteLine("========================");
+Console.WriteLine("Задание №2");
+Console.WriteLine("========================");
 
+Console.WriteLine(StringReverse("АТМТА")) ;
 
 
 
