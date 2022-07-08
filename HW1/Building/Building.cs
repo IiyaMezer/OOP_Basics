60832fa4cdf52c8889ad7f2057c9f3609b10594f
Ilya Mezer <mr.mezer@myttk.ru>
2022-07-08 20:08:50 +0700

    Расчет высоты этажа

---
+++ HW1/Building/Building.cs
@@ -0,0 +1,79 @@
+﻿namespace HW_4.Building;
+
+public class Building
+{
+    static int counter = 0;
+
+    private int _BulidHeight;
+    private int _FloorAmount;
+    private int _FlatAmount;
+    private int _EntranceAmount;
+    private int _BuildNum;
+
+    #region Свойства полей
+    public int BuildHeight
+    {
+        get => _BulidHeight;
+        set => _BulidHeight = value;
+    }
+
+    public int FloorAmount
+    {
+        get => _FloorAmount;
+        set => _FloorAmount = value;
+    }
+
+    public int FlatAmount
+    {
+        get => _FlatAmount;
+        set => _FlatAmount = value;
+    }
+
+    public int EntranceAmount
+    {
+        get => _EntranceAmount;
+        set => _EntranceAmount = value;
+    }
+
+    public int BuildNum
+    {
+        get => _BuildNum;
+    }
+    #endregion
+      
+
+    #region Methods
+    private void BuildCounter()
+    {
+        counter++;
+        _BuildNum = counter;
+    }
+    /// <summary>
+    /// Высота этажа, принебрегаем наличием верхнего и нижнего сервисного этажа.
+    /// </summary>
+    /// <param name="Build">Здание</param>
+    /// <returns></returns>
+    public static double FloorHeight(Building Build)
+    {
+        return ((double)Build.BuildHeight / Build.FloorAmount);
+    }
+    #endregion
+
+    public Building(int bulidHeight, int floorAmount, int flatAmount, int entranceAmount)
+    {
+        this._BulidHeight = bulidHeight;
+        this._FloorAmount = floorAmount;
+        this._FlatAmount = flatAmount;
+        this._EntranceAmount = entranceAmount;
+
+        BuildCounter();
+    }
+
+
+
+
+
+
+}
+
+
--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using HW_4.Building;
+
+Building Build1 = new (25,9,36,3);
+
+Console.WriteLine("Высота этажа в доме {0} : {1} м. ",Build1.BuildNum,Building.FloorHeight(Build1));
+
