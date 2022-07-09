1d8c2f2192ae95620c19f66625434018c8051faf
Ilya Mezer <mr.mezer@myttk.ru>
2022-07-09 18:59:04 +0700

    поля+конструктор

--- HW1/Class/RatNum.cs
+++ HW1/Class/RatNum.cs
@@ -4,5 +4,27 @@ namespace HW_5.Class;
 
 public class RatNum
 {
+    private int _N; //числитель
+    private int _D; //знаменатель
+
+    public int N
+    {
+        get => _N;
+        set => _N = value;
+    }
+
+    public int D
+    {
+        get => _D;
+        set => _D = value;
+    }
+
+    public RatNum (int n, int d)
+    {
+        this._N = n;
+        this._D = d;
+    }
+
+
 
 }
