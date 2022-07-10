4ef693381366b20477d3c15f9bf9f9e91736e175
Ilya Mezer <mr.mezer@myttk.ru>
2022-07-10 09:47:12 +0700

    конструктор + ToString

--- HW1/Math/Complex.cs
+++ HW1/Math/Complex.cs
@@ -2,5 +2,28 @@
 
 public class Complex
 {
+    private double _Re;
+    private double _Im;
 
+    public double Re
+    {
+        get => _Re;
+        set => _Re = value;
+    }
+    public double Im
+    {
+        get => _Im;
+        set => _Im = value;
+    }
+
+    public Complex(double Re, double Im)
+    {
+        this._Re = Re;
+        this._Im = Im;
+    }
+
+    public override string ToString()
+    {
+        return Re + "+" +"i"+ Im;
+    }
 }
