3be693394ca2f1c9a734d50d147650cfa1d7d11a
Ilya Mezer <mr.mezer@myttk.ru>
2022-07-12 11:16:39 +0700

    / добавлен

--- Math/Complex.cs
+++ Math/Complex.cs
@@ -35,6 +35,11 @@ public class Complex
         return new Complex((C1.Re*C2.Re)-(C1.Im * C2.Im), (C1.Im * C2.Re) + (C1.Re * C2.Im));
     }
 
+    public static Complex operator /(Complex C1, Complex C2)
+    {
+        return new Complex(((C1.Re*C2.Re)+(C1.Im * C2.Im))/((C2.Re*C2.Re)+(C2.Im * C2.Im)), ((C1.Im * C2.Re) - (C1.Re * C2.Im)) / ((C2.Re * C2.Re) + (C2.Im * C2.Im)));
+    }
+
     public bool Equals(Complex c1)
     {
         return (c1.Re == Re && c1.Im==Im); ;
