aa9e3eb99447324c69065e9255af33baddb8541d
Ilya Mezer <mr.mezer@myttk.ru>
2022-07-12 10:10:48 +0700

    Дз готово.

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -3,11 +3,13 @@
 
 Account Acc1 = new(10600000M, TypeEnum.VIP);
 Account Acc2 = new(5000000M, TypeEnum.Normal);
-Account Acc3 = new(10M, TypeEnum.Corp);
+Account Acc3 = new(10M, TypeEnum.Comp);
 Account Acc4 = new(10M, TypeEnum.Comp);
 Console.WriteLine(Acc1);
 Console.WriteLine();
 Console.WriteLine(Acc2);
+
+Console.WriteLine(Acc4 == Acc3);
 
 
 
