d094491560f85eede3929ed142adb1022a020449
Ilya Mezer <mr.mezer@myttk.ru>
2022-07-12 11:08:38 +0700

    Начал доп задание

---
+++ Geometry/Class1.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Geometry
+{
+    public class Figure
+    {
+        private int _Color; //пока так
+
+        private bool _Invisibility;
+
+        private float _X;
+
+        private float _Y;
+
+        public float MoveX(float x)
+        {
+            return _X += x;
+        }
+        public float MoveY(float y)
+        {
+            return _Y += y;
+        }
+    }
+}
