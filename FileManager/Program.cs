79f6f385538450f068740d73cc84c5a5a4f65bf8
Elijah_M <mr.mezer@myttk.ru>
2022-07-20 12:38:20 +0700

    Заготовка для  Create dir

--- FileManager/Program.cs
+++ FileManager/Program.cs
@@ -1,10 +1,13 @@
 ﻿
 using FileManager;
 
+
+
+
 ConsoleUserInterface UI = new();
 
 FileManagerLogic manager = new(UI);
 
 manager.Start();
 
-Console.WriteLine("END...");
+Console.WriteLine("Конец программы....");
---
+++ FileManager/Сommands/CreateDir.cs
@@ -0,0 +1,12 @@
+﻿
+using FileManager.Сommands.Base;
+
+namespace FileManager.Сommands;
+public class CreateDir : Command
+{
+    public override string Description => "Создать папку в текущей директории.";
+    public override void Execute(string[] args)
+    {
+     //to do
+    }
+}
