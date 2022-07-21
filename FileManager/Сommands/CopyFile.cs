8cccf4d02a05520981523ab0b3b669b3007d8848
Elijah_M <mr.mezer@myttk.ru>
2022-07-21 19:08:21 +0700

    копирование файлов

--- FileManager/Сommands/CopyFile.cs
+++ FileManager/Сommands/CopyFile.cs
@@ -15,33 +15,81 @@ public class CopyFile : Command
         _FileManager = FileManager;
     }
 
+    /// <summary>
+    /// Убрать из имени файла формат.
+    /// </summary>
+    /// <param name="str"имя файла></param>
+    /// <returns>имя файла без формата</returns>
+    private string RemoveEnd(string str)
+    {
+        if (str.Length < 4)
+        {
+            return string.Empty;
+        }
+
+        return str[..^4];
+    }
+
+    /// <summary>
+    /// Добваление в имя файла суффикса, для обозначения копии.
+    /// </summary>
+    /// <param name="flie">файл</param>
+    /// <returns>суффикс</returns>
+    private string AddSuff(FileInfo file)
+    {
+
+        string name = RemoveEnd(file.Name) ;
+        string suff = "";
+
+        if (file.Exists)
+        {
+            while (name == RemoveEnd(file.Name))
+            {
+                suff += "(copy)";
+                name += suff;
+            }
+        }
+            
+        return suff;
+    }
+    /// <summary>
+    /// Добавляет в файл формат.
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    private string AddFormat(string str)
+    {
+        
+        return str.Substring(str.Length - 4);
+    }
     public override void Execute(string[] args)
     {
         if (string.IsNullOrWhiteSpace(args[1]))
         {
-            _UserInterface.Writeline("Необходимо указать имя папки.");
+            _UserInterface.Writeline("Необходимо указать имя копируемого файла.");
             return;
         }
 
-        var file_path = args[2];
         var file_source = args[1];
 
         FileInfo file;
 
-        if (!Path.IsPathRooted(file_path) && !Path.IsPathRooted(file_source))
+        if (!Path.IsPathRooted(file_source))
         {
-            file_path = Path.Combine(_FileManager.CurrDir.FullName, file_path);
             file_source = Path.Combine(_FileManager.CurrDir.FullName, file_source);
 
         }
 
         file = new FileInfo(file_source);
 
-        if (file.Exists)
+        if (!file.Exists)
         {
-            file.CopyTo(file_path + file.Name + "(copy)", true);
+            _UserInterface.Writeline("Файла не существует");
+            return;
         }
+        file.CopyTo(RemoveEnd(file.Name) + AddSuff(file) + AddFormat(file.Name), true);
 
-        _UserInterface.Writeline($"Файл {file.FullName} скопирован по указнному пути.");
+
+        _UserInterface.Writeline($"Файл {file.Name} скопирован в текущий каталог с суффиксом {AddSuff(file)}.");
     }
 }
