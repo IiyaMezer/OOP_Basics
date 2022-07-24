c7feb6482a76d0096f36a18909a333638a5e5d8a
Elijah_M <mr.mezer@myttk.ru>
2022-07-24 11:06:46 +0700

    Заготовка под выод инфы о тексте

---
+++ FileManager/Сommands/PrintTextInfo.cs
@@ -0,0 +1,20 @@
+﻿using FileManager.Сommands.Base;
+
+namespace FileManager.Сommands; 
+public class PrintTextInfo : Command
+{
+    private readonly IUserInterface _UserInterface;
+    private readonly FileManagerLogic _FileManager;
+    public override string Description => "вывод информации о тектовом файле.";
+
+    public PrintTextInfo(IUserInterface UserInterface, FileManagerLogic FileManager)
+    {
+        _UserInterface = UserInterface;
+        _FileManager = FileManager;
+    }
+
+    public override void Execute(string[] args)
+    {
+        throw new NotImplementedException();
+    }
+}
